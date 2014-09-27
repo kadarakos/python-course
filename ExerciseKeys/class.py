4e7e67be859b5f00f72db8ee30d1c5fdd04e3ced
Mike Kestemont <mike.kestemont@gmail.com>
2014-09-27 16:24:06 +0200

    removed previous ex. keys

--- ExerciseKeys/class.py
+++
@@ -1,7 +0,0 @@
-words = ['bicycle', 'radar', 'origin', 'tie', 'level', 'poop', 'solar', 'nun']
-palindromes = []
-for word in words:
-	rev_w = "".join(reversed(word))
-	if rev_w == word:
-		palindromes.append(word)
-print(sorted(palindromes))
